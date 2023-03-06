2feab8cb76505c7590fbdc25c4e642f80948222a
Alexander Mendivil <alexanderima@outlook.es>
2023-03-05 18:38:31 -0700

    init

---
+++ components/CounterDay.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="container">
+
+        <div class="subcontainer">
+            <p>{{ days }}</p>
+            <p>Días</p>
+
+        </div>
+
+        <div class="subcontainer">
+            <p>{{ hours }}</p>
+            <p>Horas</p>
+
+        </div>
+
+        <div class="subcontainer">
+            <p>{{ minutes }}</p>
+            <p>Minutos</p>
+
+        </div>
+
+        <div class="subcontainer">
+            <p>{{ seconds }}</p>
+            <p>Segundos</p>
+
+        </div>
+    </div>    
+</template>
+
+<script lang="ts">
+export default {
+    data(){
+        return{
+            days: 0,
+            hours: 0,
+            minutes: 0,
+            seconds: 0,
+        }
+    },
+
+    methods:{
+
+        countDays(){
+            const second = 1000
+            const minute = second * 60
+            const hour = minute * 60
+            const day = hour * 24
+            const countDate = new Date('Feb 16, 2024 00:00:00').getTime()
+            let gap
+            let now
+            const cb  = () => {
+                now = new Date().getTime()
+                gap = countDate - now
+                this.days = Math.floor( gap / day )
+                this.hours = Math.floor( (gap % day) / hour )
+                this.minutes = Math.floor( (gap % hour) / minute )
+                this.seconds = Math.floor( (gap % minute) / second )  
+            }
+
+            setInterval(cb, 1000)
+
+        },
+    },
+    mounted(){
+        this.countDays()
+    }
+}
+</script>
+
+<style scoped>
+    .container{
+        display: flex;
+        align-items: center;
+    }
+
+    p{
+        font-size: 1.4em;
+    }
+    .subcontainer{
+        display: flex;
+        flex-direction: column;
+        justify-content: space-evenly;
+        align-content: center;
+        align-items: center;
+        margin: 0.5em;
+    }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="background-image flex">
+    
+    <div>
+      <h1>Save the date!</h1>
+      <h3>16 de febrero de 2024</h3>
+    </div>
+
+    <div>
+      <CounterDays/>
+    </div>
+
+  </div>
+</template>
+
+<script>
+import { defineAsyncComponent } from 'vue'
+export default {
+  name: 'IndexPage',
+  components:{
+    CounterDays: defineAsyncComponent(() => import('@/components/CounterDay.vue'))
+  }
+}
+</script>
+
+<style scoped>
+  .background-image{
+      background-image: url('@/assets/images/marr.jpg');
+      height: 100vh;
+      width: 100%;
+      background-position: center;
+      background-repeat: no-repeat;
+      background-size: cover;
+  }  
+  .flex{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: center;
+  }
+</style>
